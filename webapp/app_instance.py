714ad4ab5161e0c46301e3e2e5fc5385865b3beb
yuhangchen <yuhangchen0425@gmail.com>
2025-12-29 15:29:17 -0500

    Fix docker mongo uri

--- webapp/app_instance.py
+++ webapp/app_instance.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
-#this file is essential for decoupling the application logic
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Tripedia"
+
+app.config["MONGO_URI"] = os.getenv(
+    "MONGO_URI",
+    "mongodb://mongo:27017/Tripedia" 
+)
+
 app.secret_key = "mysecretkey"
-
-
-
-
